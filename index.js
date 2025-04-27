e82383502ce6d969862cae155c7b10c87376185c
Nitish Gupta <nitishiit2012@gmail.com>
2025-04-27 22:17:54 +0530

    add to github

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+
+const app=express();
+
+const port = 3000;
+
+app.use(express.json());
+
+const teaData=[];
+var nextId=1;
+app.post("/tea",(req,res)=>{
+    const {name ,price} = req.body;
+    const tea={id: nextId++,name,price};
+    teaData.push(tea);
+    res.status(201).send(tea);
+});
+
+app.get("/teas",(req,res)=>{
+    res.status(200).send(teaData);
+});
+
+app.get("/teas/:id",(req,res)=>{ 
+
+    const id =parseInt(req.params.id);
+    const tea = teaData.find((t)=>t.id===id);
+    if(!tea){
+        return res.status(404).send("tea not found");
+    }
+    res.status(200).send(tea);
+ });
+
+ app.put("/teas/:id",(req,res)=>{
+    const id =parseInt(req.params.id);
+    const tea = teaData.find((t)=>t.id===id);
+    if(!tea){
+        return res.status(404).send("tea not found");
+    }
+    const {name,price}=req.body;
+    tea.name=name;
+    tea.price=price;
+    res.status(200).send(tea);
+ });
+ app.delete("/teas/:id",(req,res)=>{
+    const id =parseInt(req.params.id);
+    const teaIndex = teaData.findIndex((t)=>t.id===id);
+    if(teaIndex===-1){
+        return res.status(404).send("tea not found");
+    }
+    teaData.splice(teaIndex,1);
+    res.status(204).send();
+ });
+app.listen(port,()=>{
+    console.log(`server running at http://localhost:${port}/`)});
